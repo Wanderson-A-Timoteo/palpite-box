3034c09a56be278921522b450ea01db3dc559b1f
Wanderson-A-Timoteo <wanderson_timoteo@hotmail.com>
2020-07-13 19:54:09 -0400

    Update pesquisa.js

--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -36,7 +36,7 @@ const Pesquisa = () => {
       <PageTitle title='Pesquisa' />
       <h1 className='text-center font-bold my-4 text-2xl'>Críticas e sugestões</h1>
       <p className='text-center mb-6'>O restaurante X sempre busca por atender melhor seus clientes.<br />
-Por isso, estamos sempre abertos a ouvir a sua opinião.</p>
+      Por isso, estamos sempre abertos a ouvir a sua opinião.</p>
       {!sucess && <div className='w-1/5 mx-auto'>
         <label className='font-bold'>Seu nome:</label>
         <input type='text' className='p-4 block shadow bg-blue-100 my-2 rounded' placeholder='Nome' onChange={onChange} name='Nome' value={form.Nome} />
